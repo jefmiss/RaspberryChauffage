e4d78bc977989c2c363b1759ab26b90c56b67fd5
jef2 <jef2@DESKTOP-7AGEE2B>
2018-12-05 22:50:21 +0100

    commit de test

--- www/test.php
+++ www/test.php
@@ -1,5 +1,8 @@
 <?php
+
 include 'ArrierePlan/configBase.inc';
 //include 'ArrierePlan/functions.inc';
 include 'inc/requetesbis.class.php';
 include 'inc/requetesSpeciales.class.php';
+
+echo "bonjours ";
